4445ec53393141401055fc4917325b48be54e579
Arthur-Diez <ishmukhamedovartur03@gmail.com>
2026-02-17 00:19:22 +0300

    fd

--- src/data/spreadSchemas.ts
+++ src/data/spreadSchemas.ts
@@ -635,8 +635,8 @@ const MANARA_LAYOUT_OPTIONS_BY_SPREAD: Partial<Record<ManaraSpreadId, LayoutScal
     scaleY: 8.8
   },
   manara_celtic_cross: {
-    scaleX: 5,
-    scaleY: 6
+    scaleX: 4.2,
+    scaleY: 4.8
   }
 };
 
